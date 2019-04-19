d6b2d2861bea7fe8e8a7f0650ff982f5b1200916
CristelleF <telmesumting@gmail.com>
2019-04-19 15:57:07 -0400

    additionalDogs

--- main.js
+++ main.js
@@ -1,4 +1,37 @@
-var i = 0;
+var s = 0;
    function buttonClick() {
-       document.getElementById('inc').value = ++i;
+       document.getElementById('salt').value = ++s;
    }
+
+   var p = 0;
+   function buttonClicks() {
+       document.getElementById('peppa').value = ++p;
+   }
+
+   var b = 0;
+   function buttonClicks() {
+       document.getElementById('Bae&Jay').value = ++b;
+   } 
+
+   var f = 0;
+   function buttonClicks() {
+       document.getElementById('fattyboys').value = ++f;
+   }
+   
+   var m = 0;
+   function buttonClicks() {
+       document.getElementById('migos').value = ++m;
+   }
+
+/*   const dog=document.querySelector("img");
+   let counter=0;
+
+const click=document.querySelector("#click");
+
+
+click.textContent=counter;
+
+   dog.addEventListener("click",()=>{
+       counter++
+       click.textContent=counter;
+   })*/
