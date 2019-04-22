cb4d99ad886e1b3dd454f6892591e65df436d436
CristelleF <telmesumting@gmail.com>
2019-04-22 10:09:22 -0400

    update

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 var s = 0;
-   function buttonClick() {
+   function buttonClick1() {
        document.getElementById('salt').value = ++s;
    }
 
    var p = 0;
-   function buttonClicks() {
+   function buttonClick2() {
        document.getElementById('peppa').value = ++p;
    }
 
    var b = 0;
-   function buttonClicks() {
+   function buttonClick3() {
        document.getElementById('Bae&Jay').value = ++b;
    } 
 
    var f = 0;
-   function buttonClicks() {
+   function buttonClick4() {
        document.getElementById('fattyboys').value = ++f;
    }
    
    var m = 0;
-   function buttonClicks() {
+   function buttonClick5() {
        document.getElementById('migos').value = ++m;
    }
 
@@ -35,3 +35,27 @@ click.textContent=counter;
        counter++
        click.textContent=counter;
    })*/
+
+   const dog1={
+       name:"Salt",
+       picture:"puppy pigtales.jpg",
+       clicks:0
+   }
+   const dog2= {
+       name:"Peppa",
+       picture:"cute-puppy-2.jpg",
+       clicks:0
+   }
+   const dog3= {
+       name:"Bae&Jay",
+       picture:"Bae&Jay.jpg",
+       clicks:0
+   }
+   const dogs=[dog1,dog2,dog3]
+
+   for(dog of dogs){
+      dog.addEventListener() 
+   }
+   function myFunction(){
+       dogs.splice(2,0,"fattyboys","migos");
+   }
